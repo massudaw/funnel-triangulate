9af5da708fd571a380ba7dcf2a37b2f650602da0
Wesley Sales Massuda <wesley.massuda@gmail.com>
2016-04-06 12:03:25 -0300

    improve

--- src/Triangulation.hs
+++ src/Triangulation.hs
@@ -2,6 +2,8 @@
 module Triangulation where
 
 import Data.Ord
+import System.IO.Unsafe
+import qualified Triangle.FFI as Tri
 import Search
 import System.Random
 import GareysTriangulation
@@ -19,6 +21,7 @@ import Data.Maybe
 import qualified Data.List as L
 import Linear.V2
 import Linear.V3
+import Linear.Vector
 import Polygon
 import Point2
 import Triangle
@@ -39,14 +42,17 @@ data PolygonSet ix a
 
 testDXF = do
   Right f <- readDXF "/home/massudaw/src/triangulate/PATH.DXF"
-  let [Entity a1 ob (LWPOLYLINE _ _ _ b _ _ )] = filter ((== "room").layer. eref) $ entities f
+  let ([Entity a1 ob (LWPOLYLINE _ _ _ b _ _ )],hs) = splitAt 1 $ reverse $ L.sortBy (comparing (abs . Polygon.area . PolygonCW .fmap (unconvV2 .fst) . projLine)) $  filter ((== "room").layer. eref) $ entities f
       door = filter ((== "origin").layer. eref) $ entities f
-      pessoa = head $ filter ((== "pessoa").layer. eref ) $ entities f
-      saida = filter ((== "saida").layer . eref ) $ entities f
+      projLine (Entity _ _ (LWPOLYLINE _ _ _ h _ _ )) = h
+      pessoas = filter ((== "pessoa").layer. eref ) $ entities f
+      saidas = filter ((== "saida").layer . eref ) $ entities f
       projC (Entity _ _  (CIRCLE(V3 tx ty _ ) _ )) = Point2 (tx,ty)
-      entity = fmap (\(V2 a b  ,_) -> Point2 (a, b)) b
-  o <- searchPaths (projC pessoa) (projC <$> saida) entity
+      entity = (fmap (unconvV2 .fst) b, fmap (fmap (unconvV2 .fst).projLine)  hs)
+      po = triangulate entity
+  o <- searchPaths (projC <$> pessoas) (projC <$> saidas) po
   let genPath o = (\s -> Entity a1 (ob {layer = "rotas", handle = s})  (LWPOLYLINE False 0 Nothing (fmap (\(Point2 (a,b)) -> (V2 a b  ,Nothing)) o ) Nothing Nothing ))
+
   writeDXF "/home/massudaw/src/triangulate/PATHW.DXF" (foldr addEntity f (genPath <$> o))
 
 
@@ -65,22 +71,20 @@ reorderpath p1 ((i,(a,b)):xs)
 reorderpath p1 [] = []
 
 -- build nearest path  using funnel and checking for target in sight
-funnel _ t apx (pl,pr) l i
-  | i +1 >= length l =  tip
+funnel _ t apx (pl,pr) xl i
+  | i  + 1 >= length xl =  tip
     where
+      (_,(l,r)) = xl !!  i
+      deg f = id -- trace (f <> show (apx,pl,pr))
       tip
-        | area2 apx pr t <= 0 &&  area2 apx pl t >= 0  && (vequal apx  pr || area2 apx pl t > 0) &&  (vequal apx pl || area2 apx pr t < 0)
-          = Just [t]
-        {-| area2 apx pr t <= 0 && area2 apx pl t >0
-          = Just [t]
-        | area2 apx pl t >= 0 && area2 apx pr t <0
-          = Just [t]-}
-        | area2 apx pr t <= 0
-           =  Just [pl,t]
-        | area2 apx pl t >= 0
-           =  Just [pr,t]
+        | area2 apx pr t <= 0 &&  area2 apx pl t >= 0
+          = deg "visible both" $ Just [t]
+        | area2 apx pr t <= 0 && area2 apx pl t < 0
+           =  deg "visible last left " $Just [l,t]
+        | area2 apx pl t >= 0&& area2 apx pr t > 0
+           = deg "visible last right" $ Just [r,t]
         | otherwise
-           = Just [t]
+           = deg "no case last" $ Just [t]
 funnel  (ir,il) t apx (pl,pr) xl ip =
     let
       cr = area2 apx pr r <= 0.0
@@ -90,26 +94,26 @@ funnel  (ir,il) t apx (pl,pr) xl ip =
       i = ip +1
       (_,(l,r)) = xl !!  i
 
-      deg f = trace (f <> show (apx,pl,pr,l,r))
+      deg f = id -- trace (f <> show (apx,pl,pr,l,r))
       ret
         | cr &&  ccr  &&  cl &&   ccl
           = deg "forwb"  $ funnel (i,i) t apx (l , r) xl i
-        |  area2 apx pr t <= 0 &&  area2 apx pl t >= 0  && (area2 apx pl t > 0) &&  (area2 apx pr t < 0)
-          = deg "visible"  $  Just [t]
-        {-| (cr &&  not ccr ) &&  (cl &&   ccl)
+        -- |  area2 apx pr t <= 0 &&  area2 apx pl t >= 0  && (area2 apx pl t > 0) &&  (area2 apx pr t < 0)
+        --  = deg "visible"  $  Just [t]
+        | (cr &&  not ccr ) &&  (cl &&   ccl)
           =
                 let il = i
                     pl = l
                     apx = pl
                     iapx = il
-                in (pl:) <$> deg "cutrfl"   (funnel (iapx,iapx) t apx (apx,apx) xl iapx )-}
-        {- | (cr &&  ccr)  &&  (cl &&   not ccl)
+                in (pl:) <$> deg "cutrfl"   (funnel (iapx,iapx) t apx (apx,apx) xl iapx )
+        | (cr &&  ccr)  &&  (cl &&   not ccl)
           =
                 let ir = i
                     pr = r
                     apx = pr
                     iapx = ir
-                in (pr:) <$> deg "cutlfr" (funnel  (iapx,iapx) t apx (apx,apx) xl iapx  )-}
+                in (pr:) <$> deg "cutlfr" (funnel  (iapx,iapx) t apx (apx,apx) xl iapx  )
         | cr
           = if ccr
               then deg "forwr"  $ funnel (i,il) t apx (pl,r) xl i
@@ -162,17 +166,23 @@ paths i t (g , l) =
             mp (e,[a,b]) = ([(a,b),(b,a)])
             mp (e,i ) = error (show i)
 
+unconvV2 (V2 a b) = Point2 (a,b)
 -- Triangulate and create a datastructure with nodes and edges
-triangulate :: [Point2 Double] -> (PolygonSet Int Double, [(Int,Triangle Point2 Double)])
-triangulate nodes =  (PolygonSet (zip [0..] nodes) (zip [0..] links) (zip [0..] trigs) ,  zip [0..] triangles)
+triangulate :: ([Point2 Double],[[Point2 Double]]) -> (PolygonSet Int Double, [(Int,Triangle Point2 Double)])
+triangulate (prenodes ,holes) =  (PolygonSet (zip [0..] nodes ) (zip [0..] links) (zip [0..] trigs) ,  zip [0..] triangles)
   where
-    triangles =  garey (PolygonCCW nodes)
-    tri (Triangle (ap,bp,cp)) = [(a,b),(b,c),(c,a)]
-      where idx  i = justError (show i) . flip L.elemIndex nodes $ i
-            a = idx ap
-            b = idx bp
-            c = idx cp
-    triidx = tri <$> triangles
+    -- nodes = prenodes
+    nodes = fmap unconvV2 $ Tri.pointlist pretriangles
+    centerHole = fmap (\hole -> (sum (convV2 <$> hole))^/ (fromIntegral $ length hole)) holes
+    totallength = (length prenodes  + sum (fmap length holes))
+    (pretriangles ,_)= traceShowId $ unsafePerformIO $ Tri.testTriangulate "pz" (traceShowId $ Tri.TriangulateIO (convV2 <$> prenodes <> concat holes) [] [] totallength 0 [] []  [] [] 0  0 0 holeSegs []    totallength centerHole  (length holes) [] 0 [] [] [] 0)
+    consEdge prenodes hole = prenodes <> fmap (fmap fromIntegral) (zipWith V2 [length prenodes ..(length prenodes + length hole )-1] ([(length prenodes +1)..(length prenodes + length hole ) -1] <> [length prenodes ]))
+    holeSegs =  foldl consEdge  [] (prenodes:holes)
+    triangles = traceShowId $ fmap (\(V3 a b c) -> Triangle (var a,var b ,var c))$ Tri.trianglelist pretriangles
+      where
+        var i = fromJust $ M.lookup i (M.fromList (zip [0..] nodes ))
+
+    triidx = (\(V3 a b c) -> [(a,b),(b,c),(c,a)]) . fmap fromIntegral <$> (Tri.trianglelist pretriangles)
     links = L.nubBy (\i j -> i == j || i == (swap j)) $ concat $ triidx
     trigs =  fmap (fmap (\i -> justError (show i) $ ((,True) <$> L.elemIndex i links)  <|> ((,False ) <$> L.elemIndex (swap i ) links )) ) triidx
 
@@ -185,8 +195,8 @@ limitrange l o (x:xs)
   | distance o x <= l  = (o, distance o x) : limitrange (l - distance o x) x xs
 
 
--- Rendering
 
+-- Rendering
 drawEdge :: Show a => (a, (Point2 Double, Point2 Double)) -> Solid
 drawEdge (i,t@(a,b@(Point2 (bx,by)))) = color (0,1,0,1) $ union (moveP b $ sphere 0.6 ) $ union (moveZ 1 $ moveP cen (scale (0.05,0.05,0.05) $ text (show i)) ) $  (extrude (Polygon (reverse [conv a, (\[i,j] -> [i -0.01,j+0.01]) (conv a) , conv b , (\[i,j] -> [i - 0.01,j+0.01]) (conv b)]) []) 0.3)
   where conv (Point2 (a,b)) = [a,b]
@@ -200,6 +210,7 @@ drawTriangle (r,(i,t@(Triangle (a,b,c)))) =  color (1,r,0,1) $ union (moveZ 1 $ 
 
 moveP (Point2 (x,y)) = move (x,y,0)
 conv (Point2 (a,b)) = [a,b]
+convV2 (Point2 (a,b)) = V2 a b
 convV3 z (Point2 (a,b)) = (a, b, z)
 
 line l = [color (0,0,1,1) $ extrude (Polygon (f <> reverse (fmap (\[i,j]-> [i-0.01 ,j+0.02]) f)) []) 0.3]
@@ -212,13 +223,14 @@ genpath po  p1 p2 = f
 
 plotLimitedLine (o,l) =    union (move (convV3 (-3) o ) $ extrude ( Circle  l) 1) (move (convV3 (0) o ) $sphere 0.6)
 
-searchPaths p1 tar poly = do
-  let po = triangulate poly
+pathDistance p =  sum $ zipWith distance p (tail p)
+
+searchPaths p1s tar po = do
   s <- getStdGen
   let r = randoms s
-  let f = catMaybes $ concat $ fmap (genpath po p1) tar
+  let f = concat $  fmap (\p1 -> catMaybes $ fmap (F.minimumBy (comparing (fmap pathDistance)) .genpath po p1) tar) p1s
   let lline = concat $ fmap (\f-> plotLimitedLine <$> limitrange 30 (head f) (tail f) )f
-  T.writeFile "test.scad" (openSCAD (Statements $ [moveP p1 (sphere 1)] <> fmap (\p2 -> moveP p2 (cube 1)) tar   <> ( concat $ concat $ fmap (fmap drawEdge ).   (\p2 -> fmap (reorderpath p1). portals $  paths p1 p2  po ) <$> tar ) <> (drawTriangle <$>  (zip r $ snd po)) <> lline <> (concat $ line <$> f)))
+  T.writeFile "test.scad" (openSCAD (Statements $ (flip moveP (sphere 1) <$> p1s) <> fmap (flip moveP (cube 1)) tar   <> {- ( concat $ concat $ fmap (fmap drawEdge ).   (\p2 -> fmap (reorderpath p1). portals $  paths p1 p2  po ) <$> tar ) <>-} (drawTriangle <$>  (zip r $ snd po)) <> (concat $ line <$> f)))
   return (f)
 
 
